17cc686cd7e77da5143b0c890f32079fec2a9b45
Michele Acierno <michele.m.acierno@gmail.com>
2024-10-30 13:03:49 +0100

    Add input validation and improve guess feedback
    Introduced a function to ensure non-negative integer input from the user. Modified the guessing loop to provide immediate feedback and track the number of attempts, including optimal number of tries based on logarithmic calculation.

--- python/student_exercises/implementations/guess_number/guess_number.py
+++ python/student_exercises/implementations/guess_number/guess_number.py
@@ -1,6 +1,23 @@
 import random
 import math
 
+
+def get_non_negative_int(prompt):
+    while True:
+        try:
+            _input_value = int(input(prompt))
+        except ValueError:
+            print("Sorry, you should enter an integer.")
+            continue
+
+        if _input_value < 0:
+            print("Sorry, your response must not be negative.")
+            continue
+        else:
+            break
+    return _input_value
+
+
 def guess_number():
     """
     Function that generates a random number between 1 and 100 and asks the user to guess it.
@@ -9,15 +26,20 @@ def guess_number():
     # for dychtomy optimal search, the formula is the following: log2(100)
 
     # Generate a random number between 1 and 100
-    secret_number = random.randint(1, 100)
+    secret_number: int  = random.randint(1, 100)
+    optimal_tries: int  = int(math.log(100, 2))
     print(secret_number)
-
-    # TODO: ask user for a number unisng `input` function /!\ hint don't forget to convert the result of input as int
-    # TODO: check if user number is equal to secret number, if not display "The guess number is higher" or "The guess number is lower" accordingly
+    trys: int  = 1
+    while (input_data := get_non_negative_int("Try you luck! Enter a number\n")) != secret_number:
+        trys += 1
+        print(f"Try again!, your secret number is {'higher' if secret_number > input_data else 'lower'}")
 
     # to ask user to enter the number use the function input
-    print("bravo")
+    print(f"bravo, you did it in {trys} {('try', 'tries')[trys > 1]}!")
+    print(f"the optimal ties are {optimal_tries} for this range, you did {('optimal!','good.')[trys > optimal_tries]}")
 
 if __name__ == '__main__':
     # Run the game
     guess_number()
+
+
