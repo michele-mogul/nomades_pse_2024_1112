57a31a87094c8d456ffa7435915c415fc500c200
Michele Acierno <michele.m.acierno@gmail.com>
2024-11-01 16:12:02 +0100

    Add full tic-tac-toe game implementation
    Complete the tic-tac-toe game by adding board drawing, player move handling, and win check logic. The game now supports random player selection, move validation, and displays win and draw messages.

--- python/student_exercises/implementations/some_small_exercises/tictactoe.py
+++ python/student_exercises/implementations/some_small_exercises/tictactoe.py
@@ -1,4 +1,6 @@
 import os
+from random import randint
+
 
 # Function to draw the tic-tac-toe board
 def draw_board(board: list[str]) -> None:
@@ -8,49 +10,67 @@ def draw_board(board: list[str]) -> None:
     Arguments:
     - board (list): List representing the tic-tac-toe board.
     """
+
+    board_str = f"""
+    |1|2|3|       {board[0]} | {board[1]} |  {board[2]}                         
+    |4|5|6|       {board[3]} | {board[4]} |  {board[5]}              
+    |7|8|9|       {board[6]} | {board[7]} |  {board[8]}                                                    
+                   
+
+    """
     os.system('cls' if os.name == 'nt' else 'clear')  # Clear the console
-    # TODO: Draw the board. PRO TIP: look at the example `tictactoe.310.py
+    print(board_str)
 
 def check_win(board: list[str], player: str) -> bool:
-    """
-    Function to check if a player has won.
-    A player wins if they have 3 consecutive marks in a row, column or diagonal.
+    #create a check board which is a list of 3 lists
 
-    Arguments:
-    - board (list): List representing the tic-tac-toe board.
-    - player (str): Player's mark ('X' or 'O').
+    win_list = [
+        set(board[0:3]), set(board[3:6]), set(board[6:9]),
+        set(board[0::3]),set(board[1::3]),set(board[2::3]),
+        set(board[0::4]),set(board[-3::-2])
+    ]
 
-    Returns:
-    - win (bool): True if the player has won, False otherwise.
-    """
-    return False
+    winner = list(filter(lambda x: player in x and len(x) == 1, win_list))
+    return len(winner) > 0
+
 
 # Function to play the game
 def play_game():
     # The board variable store the state of the game, where board[0] is the top left corner and board[8] is the bottom right corner
     board = [' '] * 9
-    current_player = None # TODO: choose player at random (the player value can be 'X' or 'O')
+
+    current_player = ('X','O')[randint(0,1)]
     # the game_over variable is used to know if the game is running or not
-    game_over = False
+    game_over  = False
 
     while not game_over:
-        # TODO: Draw the board
+        draw_board(board)
+        move = input(f"Its player {current_player} move. Enter the number of the cell:")
 
-        # TODO: Get player's move
-        # Hint: Use input() to get the move from the player
-        move = None
+        try:
+            val = int(move)
+            if val < 1 or val > 9:
+                raise ValueError
+            if board[val - 1] != ' ':
+                raise ValueError
+            else:
+                board[val-1] = current_player
+                draw_board(board)
+                if check_win(board, current_player):
+                    print(f"Player {current_player} wins!")
+                    game_over = True
 
-        # TODO: Check if move is valid
-        # A valid move is an integer between 1 and 9 (both inclusive)
-        # And the board for this integer is empty
-        # if move is not valid, print "Invalid move. Try again!" and ask for a new moove
+                if ' ' not in board:
+                    print("Draw!")
+                    game_over = True
 
-        # TODO: Update the board with the move
-        # The borad is the list named board that contains state of the game
-        
-        # TODO: Check if the current player has won or draw or continue
-        # Hint: Use the check_win() function to check if the current player has won
-        # You need to think about something for the draw case
-        # if no win and no draw the game continues, switch user 'X'->'O' || 'O'->'X'
+            current_player = ('X', 'O')[current_player == 'X']
+
+
+        except ValueError:
+            print("Invalid move. Try again!")
+            continue
+
 # Start the game
-play_game()
+if __name__ == "__main__":
+    play_game()
